3c859f8fd9f56c52fa7af71f9824bd2af0803bc7
leonardofidelis@youbrtech.com.br <leofipp@hotmail.com>
2025-01-29 17:40:10 -0300

    novos cap

--- cap5-operacoes/operacoes.go
+++ cap5-operacoes/operacoes.go
@@ -1,7 +1,20 @@
 package main
 
 import "fmt"
+import "time"
 
+
+type Idade struct {
+    anoNascimento int
+}
+
+func (i Idade) Calcular() int {
+    return time.Now().Year() - i.anoNascimento
+}
+
+func (i Idade) String() string{
+    return fmt.Sprintf("Idade desde %d",i.anoNascimento)
+}
 type Operacao interface {
     Calcular() int
 }
@@ -30,15 +43,7 @@ func (s Subtracao) String() string{
     return fmt.Sprintf("%d - %d", s.operando1,s.operando2)
 }
 
-func main(){
-
-    operacoes := make([]Operacao,4)
-
-    operacoes[0] = Soma{10,20}
-    operacoes[1] = Subtracao{30,15}
-    operacoes[2] = Subtracao{10,50}
-    operacoes[3] = Soma{5,2}
-
+func acumular (operacoes []Operacao) int{
     acumulador := 0
 
     for _,op := range operacoes{
@@ -47,5 +52,25 @@ func main(){
         acumulador += valor
     }
 
-    fmt.Println("Valor acumulado =",acumulador)
+    return acumulador
+}
+
+func main(){
+
+    operacoes := make([]Operacao,4)
+
+    operacoes[0] = Soma{10,20}
+    operacoes[1] = Subtracao{30,15}
+    operacoes[2] = Subtracao{10,50}
+    operacoes[3] = Soma{5,2}
+    fmt.Println("Valor acumulado =",acumular(operacoes))
+
+
+    idades:=make([]Operacao,3)
+    idades[0] = Idade{1969}
+    idades[1] = Idade{1950}
+    idades[2] = Idade{1999}
+    fmt.Println("Idades acumulada =",acumular(idades))
+
+
 }
